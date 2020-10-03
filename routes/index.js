52fcae18d2f57dd487f1c0133a865eb30da7bf9c
Madhur Shrimal <madhur.shrimal@gmail.com>
2020-10-02 18:00:40 -0700

    qna working

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,41 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', (req, res, next) => {
+  var errors = []
+  if (!req.body.question) {
+    errors.push("No question specified")
+  }
+
+  if (!req.body.answer) {
+    errors.push("No answer specified")
+  }
+
+  if (errors.length){
+    res.status(400).json({"error":errors.join(",")});
+    return;
+  }
+
+  var data = {
+    question: req.body.question,
+    answer: req.body.answer
+  }
+
+  var sql ='INSERT INTO qanda (question, answer) VALUES (?,?)'
+  var params =[data.question, data.answer]
+
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(500).json({"error": err.message})
+      return;
+    }
+
+    res.redirect('/')
+  })
+})
+
+router.get('/create', (req, res, next) => {
+  res.render('create.pug')
+})
+
 module.exports = router;
